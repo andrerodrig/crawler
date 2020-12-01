c01a5d35963e66acaf046ec368a823c7df67bc03
André Rodrigues <43825622+andrerodrig@users.noreply.github.com>
2020-12-01 11:03:34 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from crawler.correios import HtmlObject
+
+
+def crawler():
+    """Collects data from the site passed"""
+    ff = webdriver.Firefox()
+    correios = HtmlObject(
+        ff,
+        'http://www.buscacep.correios.com.br/sistemas/buscacep/buscaFaixaCep.cfm')
+    correios.search_url()
+    print("Buscando dados em todas as UFs")
+    correios.search_all_ufs()
+    print("Concluido!")
