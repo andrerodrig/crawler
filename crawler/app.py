d64e71fd3dde38c7bf46f3e81a8f1970a07ddd89
André Rodrigues <43825622+andrerodrig@users.noreply.github.com>
2020-12-01 11:02:39 -0300

    Update app.py

--- crawler/app.py
+++ crawler/app.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
-from crawler.correios import Correios
+from crawler.correios import HtmlObject
 
 
 def crawler():
     """Collects data from the site passed"""
     ff = webdriver.Firefox()
-    correios = Correios(
+    correios = HtmlObject(
         ff,
         'http://www.buscacep.correios.com.br/sistemas/buscacep/buscaFaixaCep.cfm')
     correios.search_url()
