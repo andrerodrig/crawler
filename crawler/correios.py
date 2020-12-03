46507bb0e126b34946b98155bbf73c12c91b20c6
andrerodrig <andrelmarques11@gmail.com>
2020-12-03 05:06:05 -0300

    Some changes in app.py and correios.py to fix import errors

--- crawler/correios.py
+++ crawler/correios.py
@@ -4,11 +4,12 @@ import pandas as pd
 
 class HtmlObject:
     """Class for the HTML element for search"""
+
     def __init__(self, driver, url):
         self.driver = driver
         self.url = url
-        self.name_select_ufs = 'UF'
-        self.tag_name_ufs = 'option'
+        self.name_select_ufs = "UF"
+        self.tag_name_ufs = "option"
         self.__ufs = None
         self.location_table_xpath = '(//table[@class="tmptabela"])[last()]'
         self.btn_search = 'input[value="Buscar"]'
@@ -23,39 +24,42 @@ class HtmlObject:
         if not self.__ufs:
             select_ufs = self.driver.find_element_by_name(self.name_select_ufs)
             options = [
-                uf.text for uf
-                in select_ufs.find_elements_by_tag_name(self.tag_name_ufs)]
+                uf.text
+                for uf in select_ufs.find_elements_by_tag_name(self.tag_name_ufs)
+            ]
 
             self.__ufs = list(filter(lambda x: bool(x) is not False, options))
         return self.__ufs
-
 
     def _search_uf(self, uf):
         """Search for a uf"""
         select_ufs = self.driver.find_element_by_name(self.name_select_ufs)
         select_ufs.find_element_by_css_selector(
-            f'{self.tag_name_ufs}[value={uf}]'
+            f"{self.tag_name_ufs}[value={uf}]"
         ).click()
         btn_search = self.driver.find_element_by_css_selector(self.btn_search)
         btn_search.click()
 
     def uf_locations(self):
-        """Creates a data frame with the fields 'Localidade' and 'Faixa de CEP'"""
-        location_table = self.driver.find_element_by_xpath(
-            self.location_table_xpath)
+        """
+        Creates a data frame with the fields 'Localidade' and 'Faixa de CEP'"""
+        location_table = self.driver.find_element_by_xpath(self.location_table_xpath)
         df_table = pd.read_html(
-            "<table>" + location_table.get_attribute('innerHTML') + "</table>")
-        return df_table[0][['Localidade', 'Faixa de CEP']]
+            "<table>" + location_table.get_attribute("innerHTML") + "</table>"
+        )
+        return df_table[0][["Localidade", "Faixa de CEP"]]
 
     def export_jsonl(self, data, filename):
         """Exports to the format jsonlines"""
-        pd.to_json(filename, orient='records', lines=True)
+        jsonl = data.to_json(orient="records", lines=True)
+        with open(filename, mode="w", encoding="utf-8") as file:
+            for line in jsonl:
+                file.write(line)
 
     def search_all_ufs(self):
         """Collect the data for all ufs"""
         for uf in self.ufs:
             self._search_uf(uf)
             df_locations = self.uf_locations()
-            breakpoint()
-            self.export_json(df_locations, f'data/{uf}.jsonl')
+            self.export_jsonl(df_locations, f"data/{uf}.jsonl")
             self.driver.back()
